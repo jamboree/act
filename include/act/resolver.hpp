0e77a2b42bb48a56ffeb0b62e04f6a626f68f0bb
jamboree <tongari95@gmail.com>
2017-08-07 15:59:12 +0800

    Fix resolver

--- include/act/resolver.hpp
+++ include/act/resolver.hpp
@@ -1,5 +1,5 @@
 /*//////////////////////////////////////////////////////////////////////////////
-    Copyright (c) 2015 Jamboree
+    Copyright (c) 2015-2017 Jamboree
 
     Distributed under the Boost Software License, Version 1.0. (See accompanying
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -14,25 +14,25 @@ namespace act
     template<class Resolver>
     inline auto resolve(Resolver& resolver, typename Resolver::query const& query)
     {
-        ACT_RETURN_FREE_AWAITER(typename Resolver::iterator, resolver, resolve, query);
+        ACT_RETURN_AWAITER(typename Resolver::iterator, resolver, resolve, query);
     }
 
     template<class Resolver>
     inline auto resolve(Resolver& resolver, typename Resolver::query const& query, error_code& ec)
     {
-        ACT_RETURN_FREE_AWAITER_EC(typename Resolver::iterator, resolver, resolve, query);
+        ACT_RETURN_AWAITER_EC(typename Resolver::iterator, resolver, resolve, query);
     }
 
     template<class Resolver>
     inline auto resolve(Resolver& resolver, typename Resolver::endpoint_type const& endpoint)
     {
-        ACT_RETURN_FREE_AWAITER(typename Resolver::iterator, resolver, resolve, endpoint);
+        ACT_RETURN_AWAITER(typename Resolver::iterator, resolver, resolve, endpoint);
     }
 
     template<class Resolver>
     inline auto resolve(Resolver& resolver, typename Resolver::endpoint_type const& endpoint, error_code& ec)
     {
-        ACT_RETURN_FREE_AWAITER_EC(typename Resolver::iterator, resolver, resolve, endpoint);
+        ACT_RETURN_AWAITER_EC(typename Resolver::iterator, resolver, resolve, endpoint);
     }
 }
 
